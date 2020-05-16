4edfceb89a1e8a4d748f815a3b3f9bb0e2d25655
Majzur <nikodem.mazur@gmail.com>
2020-05-16 19:32:22 +0200

    Add App.config and cs file for new experiments

---
+++ Akka.NetTests/ActorsWithDIAndAspectOrientedDiagnosticsTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.NetTests
+{
+    public class ActorsWithDIAndAspectOrientedDiagnosticsTests : TestKit.Xunit2.TestKit
+    {
+
+    }
+}
